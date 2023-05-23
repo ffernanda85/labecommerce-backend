5fce1b4d631855d9c96b5bd8c12989dd1edc1f0f
ffernanda85 <f.fernanda85@gmail.com>
2023-05-23 00:13:45 -0300

    concluido o exercicio 02

--- exercicios/process-argv.js
+++ exercicios/process-argv.js
@@ -1,4 +1,5 @@
-const name = "Flávia"
-const age = 37
+const name = process.argv[2]
+const age = process.argv[3]
+const profession = process.argv[4]
 
-console.log(`Hi, my name is ${name} and my age is ${age} years.`);
+console.log(`Hi, my name is ${name}, I have ${age} years and my profession is ${profession}.`);
