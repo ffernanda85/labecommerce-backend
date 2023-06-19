6a7c9579ed10435107001f265c7ca2c19fb30314
ffernanda85 <f.fernanda85@gmail.com>
2023-06-19 16:05:17 -0300

    Develop: refatorada a função updateProductById para uma pasta externa

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import { products, users } from "./dataBase";
 import express, { Request, Response } from "express";
 import cors from "cors";
-import { TProduct, TUser } from "./types";
 import { getAllUsers } from "./endpoints/getAllUsers";
 import { getAllProducts } from "./endpoints/getAllProducts";
 import { createUser } from "./endpoints/createUser";
 import { createProduct } from "./endpoints/createProduct";
 import { deleteUserById } from "./endpoints/deleteUserById";
 import { deleteProductById } from "./endpoints/deleteProductById";
+import { updateProductById } from "./endpoints/updateProductById";
 
 //criando o servidor express
 const app = express();
@@ -31,76 +30,15 @@ app.get("/ping", (req: Request, res: Response) => {
 
 //Get All Users
 app.get("/users", getAllUsers);
-
 //Get All Products
 app.get("/products", getAllProducts);
-
 //create User
 app.post("/users", createUser);
-
 //create Product
 app.post("/products", createProduct);
-
 //delete User By Id
 app.delete("/users/:id", deleteUserById);
-
 //delete Product By Id
 app.delete("/products/:id", deleteProductById);
-
 //update Product By Id
-app.put("/products/:id", (req: Request, res: Response) => {
-    try {
-        const id = req.params.id;
-
-        const findProduct = products.find(product => product.id === id)
-
-        if (!findProduct) {
-            res.status(404)
-            throw new Error("Product not found");
-        }
-        //Desestruturando as entradas do req.body
-        const { name, price, description, imageUrl } = req.body
-
-        if (name !== undefined && typeof name !== "string") {
-            res.status(400)
-            throw new Error("Invalid 'name'. Enter a string");
-        }
-
-        if (price !== undefined) {
-          if (typeof price !== "number") {
-            res.status(400)
-            throw new Error("Invalid 'price'. Enter a number");  
-          } else if (price <= 0) {
-            res.status(400)
-            throw new Error("Invalid 'price'. Enter a value greater than zero");  
-          }
-        }  
-
-        if (description !== undefined && typeof description !== "string") {
-            res.status(400)
-            throw new Error("Invalid 'description'. Enter a string");
-        }
-
-        if (imageUrl !== undefined && typeof imageUrl !== "string") {
-            res.status(400)
-            throw new Error("Invalid 'imageURL'. Enter a string");
-        }
-
-        findProduct.name = name || findProduct.name
-        findProduct.price = price || findProduct.price
-        findProduct.description = description || findProduct.description
-        findProduct.imageUrl = imageUrl || findProduct.imageUrl
-
-        res.status(200).send("Update product")
-
-    } catch (error) {
-        if (res.statusCode === 200) {
-            res.status(500);
-          }
-          if (error instanceof Error) {
-            res.send(error.message);
-          } else {
-            res.send("Unexpected error!");
-          }
-    }
-});
+app.put("/products/:id", updateProductById);
