2cdb1d1506ed815e7d9b4b58fc4caf869565553f
ffernanda85 <f.fernanda85@gmail.com>
2023-06-19 15:40:11 -0300

    Develop: refatorada a função deleteProductById para uma pasta externa

---
+++ src/endpoints/deleteProductById.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { products } from "../dataBase";
+
+export function deleteProductById(req: Request, res: Response) {
+    try {
+        //atribuindo a constante id o path params id
+        const id = req.params.id;
+    
+        //verificando se o produto existe
+        const findIndexProduct = products.findIndex((product) => product.id === id);
+        if (findIndexProduct < 0) {
+          res.status(404);
+          throw new Error("Product not found");
+        }
+        //deletando o produto
+        products.splice(findIndexProduct, 1);
+        res.status(200).send("Deleted product");
+          
+      } catch (error) {
+        if (res.statusCode === 200) {
+          res.status(500);
+        }
+        if (error instanceof Error) {
+          res.send(error.message);
+        } else {
+          res.send("Unexpected error!");
+        }
+      }
+}
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { getAllProducts } from "./endpoints/getAllProducts";
 import { createUser } from "./endpoints/createUser";
 import { createProduct } from "./endpoints/createProduct";
 import { deleteUserById } from "./endpoints/deleteUserById";
+import { deleteProductById } from "./endpoints/deleteProductById";
 
 //criando o servidor express
 const app = express();
@@ -44,34 +45,7 @@ app.post("/products", createProduct);
 app.delete("/users/:id", deleteUserById);
 
 //delete Product By Id
-app.delete("/products/:id", (req: Request, res: Response) => {
-  try {
-    //atribuindo a constante id o path params id
-    const id = req.params.id;
-
-    //verificando se o produto existe
-    const findIndexProduct = products.findIndex((product) => product.id === id);
-
-    if (findIndexProduct < 0) {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-    
-    //deletando o produto
-    products.splice(findIndexProduct, 1);
-    res.status(200).send("Deleted product");
-      
-  } catch (error) {
-    if (res.statusCode === 200) {
-      res.status(500);
-    }
-    if (error instanceof Error) {
-      res.send(error.message);
-    } else {
-      res.send("Unexpected error!");
-    }
-  }
-});
+app.delete("/products/:id", deleteProductById);
 
 //update Product By Id
 app.put("/products/:id", (req: Request, res: Response) => {
