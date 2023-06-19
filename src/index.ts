c426238f85f771fe29d1c2d9062d9af29496344b
ffernanda85 <f.fernanda85@gmail.com>
2023-06-19 15:35:33 -0300

    Develop: refatorada a função deleteUserById para uma pasta externa

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { getAllUsers } from "./endpoints/getAllUsers";
 import { getAllProducts } from "./endpoints/getAllProducts";
 import { createUser } from "./endpoints/createUser";
 import { createProduct } from "./endpoints/createProduct";
+import { deleteUserById } from "./endpoints/deleteUserById";
 
 //criando o servidor express
 const app = express();
@@ -40,32 +41,7 @@ app.post("/users", createUser);
 app.post("/products", createProduct);
 
 //delete User By Id
-app.delete("/users/:id", (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    
-    //verificando se a conta existe
-    const findIndexUser = users.findIndex((user) => user.id === id);
-        
-    if (findIndexUser < 0) {
-      res.status(404);
-      throw new Error("Account not found");
-    }
-    //deleta o usuário que está no indice indicado pelo findIndexUser
-    users.splice(findIndexUser, 1);
-    res.status(200).send("Deleted user");
-
-  } catch (error) {
-    if (res.statusCode === 200) {
-      res.status(500);
-    }
-    if (error instanceof Error) {
-      res.send(error.message);
-    } else {
-      res.send("Unexpected error!");
-    }
-  }
-});
+app.delete("/users/:id", deleteUserById);
 
 //delete Product By Id
 app.delete("/products/:id", (req: Request, res: Response) => {
