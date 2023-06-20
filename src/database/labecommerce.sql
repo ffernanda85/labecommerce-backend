f9aa8aafe38631a9cd3750d7907c2fd02366743a
ffernanda85 <f.fernanda85@gmail.com>
2023-06-20 16:06:22 -0300

    Develop: criação da tabela users e inserção de dados

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -9,5 +9,13 @@ CREATE TABLE users (
     created_at TEXT NOT NULL
 );
 
+INSERT INTO users (id, name, email, password, created_at)
+VALUES
+    ("u001", "user001", "user001@gmail.com", "user001", "20/06/2023"),
+    ("u002", "user002", "user002@gmail.com", "user002", "20/06/2023"),
+    ("u003", "user003", "user003@gmail.com", "user003", "20/06/2023"),
+    ("u004", "user004", "user004@gmail.com", "user004", "20/06/2023"),
+    ("u005", "user005", "user005@gmail.com", "user005", "20/06/2023");
+
 --Exibir tabela users
 SELECT * FROM users;
