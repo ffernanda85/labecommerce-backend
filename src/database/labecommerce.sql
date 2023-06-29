45bee151117d1e1d0523cade4fdf0c7c5925f97e
ffernanda85 <f.fernanda85@gmail.com>
2023-06-29 12:08:54 -0300

    Modified: Feito ajustes nas queries de criação de tabelas

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -3,31 +3,33 @@
 -- =================== QUERIES TABLE USERS =====================
 
 --Query de criação da tabela de users
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    created_at TEXT NOT NULL
+    created_at TEXT DEFAULT(DATETIME('now', 'localtime')) NOT NULL
 );
 
+DROP TABLE users;
+
 --Query para popular a tabela users
-INSERT INTO users (id, name, email, password, created_at)
+INSERT INTO users (id, name, email, password)
 VALUES
-    ("u001", "user001", "user001@gmail.com", "user001", "20/06/2023"),
-    ("u002", "user002", "user002@gmail.com", "user002", "20/06/2023"),
-    ("u003", "user003", "user003@gmail.com", "user003", "20/06/2023"),
-    ("u004", "user004", "user004@gmail.com", "user004", "20/06/2023"),
-    ("u005", "user005", "user005@gmail.com", "user005", "20/06/2023");
+    ("u001", "user001", "user001@gmail.com", "user001"),
+    ("u002", "user002", "user002@gmail.com", "user002"),
+    ("u003", "user003", "user003@gmail.com", "user003"),
+    ("u004", "user004", "user004@gmail.com", "user004"),
+    ("u005", "user005", "user005@gmail.com", "user005");
 
 -- Query Get All Users
 -- Query para mostrar tabela users
 SELECT * FROM users;
 
 -- Query Create User
-INSERT INTO users (id, name, email, password, created_at)
+INSERT INTO users (id, name, email, password)
 VALUES
-    ("u006", "user006", "user006@gmail.com", "user006", "21/06/2023");
+    ("u006", "user006", "user006@gmail.com", "user006");
 
 --Delete user by id
 DELETE FROM users
@@ -37,7 +39,7 @@ WHERE id = "u006";
 -- =================== QUERIES PRODUCTS ==========================
 
 -- Query de criação da tabela products
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
     price REAL NOT NULL,
@@ -62,7 +64,7 @@ SELECT * FROM products;
 
 -- Query Get All Products com termo de busca
 SELECT * FROM products
-WHERE name LIKE '%game%';
+WHERE LOWER(name) LIKE LOWER('%game%');
 
 -- Query Create Product
 INSERT INTO products (id, name, price, description, image_url)
@@ -83,27 +85,29 @@ WHERE id = "p006";
 
 -- ============================== Queries Purchases =========================
 --purchases table creation query
-CREATE TABLE purchases (
+CREATE TABLE IF NOT EXISTS purchases (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     buyer TEXT NOT NULL,
     total_price REAL NOT NULL,
-    created_at TEXT NOT NULL,
+    created_at TEXT DEFAULT(DATETIME('now', 'localtime')) NOT NULL,
     FOREIGN KEY (buyer) REFERENCES users(id)
 );
 
 --Populating purchases table
-INSERT INTO purchases (id, buyer, total_price, created_at)
+INSERT INTO purchases (id, buyer, total_price)
 VALUES
-    ("b001", "u001", 10000, "26/06/2023"),
-    ("b002", "u001", 20000, "28/06/2023"),
-    ("b003", "u002", 30000, "25/06/2023"),
-    ("b004", "u003", 40000, "25/06/2023");
+    ("pur001", "u001", 10000),
+    ("pur002", "u001", 20000),
+    ("pur003", "u002", 30000),
+    ("pur004", "u003", 40000);
+
+DROP TABLE purchases;
 
 -- Query UPDATE purchases
 UPDATE purchases
 SET
     total_price = 9900
-WHERE id = "b001";
+WHERE id = "pur001";
 
 --Query INNER JOIN purchases and users
 SELECT 
@@ -115,4 +119,5 @@ SELECT
     purchases.created_at
 FROM purchases
 INNER JOIN users
-ON purchases.buyer = users.id;   
+ON purchases.buyer = users.id
+WHERE users.id = "u002";   
