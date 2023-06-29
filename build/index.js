cf202bd1d7e094149389d8eea387bf2fca95e637
Flávia Santos <114631584+ffernanda85@users.noreply.github.com>
2023-06-29 11:32:21 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dataBase_1 = require("./dataBase");
-console.log((0, dataBase_1.createProduct)("p003", "Product 003", 1900, "Melhor Product 003 do Mercado", "https://picsum.photos/seed/Monitor/401"));
-console.log((0, dataBase_1.searchProductByName)("PR"));
-//# sourceMappingURL=index.js.map
