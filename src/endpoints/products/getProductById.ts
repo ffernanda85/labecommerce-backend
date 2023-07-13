b9a48ce2c5dce1371520205b46b0a4f17d31aef8
ffernanda85 <f.fernanda85@gmail.com>
2023-07-12 22:52:32 -0300

    Modified: Ajustes identação

--- src/endpoints/products/getProductById.ts
+++ src/endpoints/products/getProductById.ts
@@ -3,16 +3,15 @@ import { db } from "../../database/knex";
 
 export async function getProductById(req: Request, res: Response) {
   try {
-      const id = req.params.id
-      const [ result ] = await db("products").where({ id: id })
-      
-      if (!result) {
-        res.status(404);
-        throw new Error("'Id' not found");
-      }
-      res.status(200).send(result)
+    const id = req.params.id;
+    const [result] = await db("products").where({ id: id });
 
-  } catch (error:unknown) {
+    if (!result) {
+      res.status(404);
+      throw new Error("'Id' not found");
+    }
+    res.status(200).send(result);
+  } catch (error: unknown) {
     //garante que o status seja alterado do padrão caso ocorra um erro inesperado
     //já que o valor do status padrão é 200
     if (res.statusCode === 200) {
